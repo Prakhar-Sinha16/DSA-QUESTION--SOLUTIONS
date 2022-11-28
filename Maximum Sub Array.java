05b56f392f688873ed6fb0b6b05ea6b7d6b24cee
Prakhar-Sinha16 <103882001+Prakhar-Sinha16@users.noreply.github.com>
2022-11-28 21:32:09 +0530

    Create Maximum Sub Array.java
    Problem Solved Successfully
    Test Cases Passed: 
    400 /400
    Total Points Scored: 
    4 /4
    Your Total Score: 
    468
    Total Time Taken: 
    1.07
    Your Accuracy: 
    33%
    Attempts No.: 
    3

---
+++ Maximum Sub Array.java
@@ -0,0 +1,85 @@
+gfg link => https://practice.geeksforgeeks.org/problems/maximum-sub-array5443/1
+
+//QUES: Find out the maximum sub-array of non negative numbers from an array.
+
+// The sub-array should be contiguous i.e., a sub-array created by choosing the second and fourth element and skipping the third element is invalid.
+
+// Maximum sub-array is defined in terms of the sum of the elements in the sub-array. Sub-array A is greater than sub-array B if sum(A) > sum(B).
+
+// Example:
+// a = [1, 2, 5, -7, 2, 3]
+// The two sub-arrays are [1, 2, 5] [2, 3].
+// The answer is [1, 2, 5] as its sum is larger than [2, 3]
+
+// NOTE: If there is a tie, then compare with segment's length and return segment which has maximum length.
+// If there is still a tie, then return the segment with minimum starting index.
+// If no such subarray is present return "-1"
+
+// Example 1:
+
+// Input:
+// n = 3
+// a[] = {1, 2, 3}
+// Output: 1 2 3
+// Explanation: In the given array every
+// element is non-negative.
+// Example 2:
+
+// Input:
+// n = 2
+// a[] = {-1, 2}
+// Output: 2
+// Explanation: The only subarray [2] is
+// the answer.
+ 
+
+// Your Task:
+// Complete the function findSubarray() which takes the array a and the size of the array, n, as input parameters and returns an array representing the answer. If there is no subarray return an array of length 1 containing -1 only. You don't to print answer or take inputs.
+
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(1)
+
+// Constraints:
+// 1 ≤ N ≤ 105
+// -105 ≤ A[i] ≤ 105
+
+//CODE: 
+class Solution {
+
+    ArrayList<Integer> findSubarray(int a[], int n) {
+        // code here
+        ArrayList<Integer>ans = new ArrayList<>();
+        int max=0;
+        int maxStart=0;
+        int maxEnd=0;
+        int cur=0;
+        int start=0;
+        int end=0;
+        for(int i=0;i<n;i++){
+            if(a[i]<0){
+                //ans.add(a[i]);
+                cur=0;
+                start=i+1;
+            }else cur+=a[i];
+            if(cur>max){
+                //ans.add(a[i]);
+                max=cur;
+                maxStart=start;
+                maxEnd=i+1;
+            }
+            else if(cur == max){
+                int curDis=i+1-start;
+                int maxDis=maxEnd-maxStart;
+                if(curDis>maxDis){
+                    maxStart=start;
+                    maxEnd = i+1;
+                }
+            }
+        }
+        for(int i = maxStart;i<maxEnd;i++){
+            ans.add(a[i]);
+        }
+        if(ans.size()==0) ans.add(-1);
+        return ans;
+    }
+}
