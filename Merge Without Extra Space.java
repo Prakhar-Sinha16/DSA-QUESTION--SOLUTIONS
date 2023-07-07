e2a76be485bbfdfd04b0cc8b879e574d0fda3599
Prakhar Sinha <103882001+Prakhar-Sinha16@users.noreply.github.com>
2023-07-07 13:00:50 +0530

    Create Merge Without Extra Space.java
    Company Tags
    Zoho, Microsoft, Snapdeal, Goldman Sachs, Adobe, Linkedin, Amdocs, Brocade, Juniper Networks, Quikr, Synopsys

---
+++ Merge Without Extra Space.java
@@ -0,0 +1,67 @@
+gfg link: https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/1
+
+QUES:
+Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order without using any extra 
+space. Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
+
+Example 1:
+
+Input: 
+n = 4, arr1[] = [1 3 5 7] 
+m = 5, arr2[] = [0 2 6 8 9]
+Output: 
+arr1[] = [0 1 2 3]
+arr2[] = [5 6 7 8 9]
+Explanation:
+After merging the two 
+non-decreasing arrays, we get, 
+0 1 2 3 5 6 7 8 9.
+Example 2:
+
+Input: 
+n = 2, arr1[] = [10 12] 
+m = 3, arr2[] = [5 18 20]
+Output: 
+arr1[] = [5 10]
+arr2[] = [12 18 20]
+Explanation:
+After merging two sorted arrays 
+we get 5 10 12 18 20.
+Your Task:
+You don't need to read input or print anything. You only need to complete the function merge() that takes arr1, arr2, n and m as input 
+parameters and modifies them in-place so that they look like the sorted merged array when concatenated.
+
+Expected Time Complexity:  O((n+m) log(n+m))
+Expected Auxilliary Space: O(1)
+
+Constraints:
+1 <= n, m <= 105
+0 <= arr1i, arr2i <= 107
+
+CODE:
+class Solution{
+    //Function to merge the arrays.
+    public static void merge(long arr1[], long arr2[], int n, int m) {
+        // code here
+        int left = n-1;
+        int right = 0;
+        
+        while(left >=0 && right < m){
+            if(arr1[left] > arr2[right]){
+                swap(arr1,left,arr2,right);
+                left--;
+                right++;
+            }else{
+                break;
+            }
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+    
+    static void swap(long a[], int index1, long b[], int index2){
+        long temp = a[index1];
+        a[index1] = b[index2];
+        b[index2] = temp;
+    }
+}
