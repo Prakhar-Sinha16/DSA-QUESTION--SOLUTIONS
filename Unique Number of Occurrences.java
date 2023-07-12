94713c10c4074d12bc3e4fdc9147391a449ea2bc
Prakhar Sinha <103882001+Prakhar-Sinha16@users.noreply.github.com>
2023-07-13 00:49:10 +0530

    Create Unique Number of Occurrences.java
    Problem Solved Successfully
    Test Cases Passed: 
    1120 /1120
    Total Points Scored: 
    2 /2
    Your Total Score: 
    1163
    Total Time Taken: 
    0.46
    
    Company Tags
    PayPal, Bloomberg, Adobe, Google, Amazon, Apple

---
+++ Unique Number of Occurrences.java
@@ -0,0 +1,59 @@
+gfg link: https://github.com/Prakhar-Sinha16/DSA-QUESTION--SOLUTIONS/new/main
+
+QUES:
+Given an array arr of N integers, the task is to check whether the frequency of the elements in the array is unique or not. Or in other 
+words, there are no two distinct numbers in array with equal frequency. If all the frequency is unique then return true, else return false.
+
+Example 1:
+
+Input:
+N = 5
+arr = [1, 1, 2, 5, 5]
+Output:
+false
+Explanation:
+The array contains 2 (1’s), 1 (2’s) and 2 (5’s), since the number of frequency of 1 and 5 are the same i.e. 2 times. Therefore, this 
+array does not satisfy the condition.
+Example 2:
+
+Input:
+N = 10
+arr = [2, 2, 5, 10, 1, 2, 10, 5, 10, 2]
+Output:
+true
+Explanation:
+Number of 1’s -> 1
+Number of 2’s -> 4
+Number of 5’s -> 2
+Number of 10’s -> 3.
+Since, the number of occurrences of elements present in the array is unique. Therefore, this array satisfy the condition.
+Your task:
+You don't need to read input or print anything. Your task is to complete the function isFrequencyUnique() which take integer N and array 
+arr of size N as arguments, and returns a boolean.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 <= N <=105
+-109 <= arr[i] <= 109
+
+CODE:
+class Solution {
+    public static boolean isFrequencyUnique(int n, int[] arr) {
+        // code here
+        HashMap<Integer,Integer>map=new HashMap<>();
+        Set<Integer>set=new HashSet<>();
+            for(int i=0;i<arr.length;i++){
+                map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            }
+            int count=0;
+            for(Map.Entry<Integer,Integer>ans:map.entrySet()){
+                set.add(ans.getValue());
+                count++;
+            }
+            if(count==set.size())return true;
+            else 
+            return false;
+    }
+}
