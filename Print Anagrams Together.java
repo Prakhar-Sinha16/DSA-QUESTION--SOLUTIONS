5a7a29e6e05b837d1cbf9fec999f667f573dc8d3
Prakhar Sinha <103882001+Prakhar-Sinha16@users.noreply.github.com>
2023-07-13 22:53:35 +0530

    Create Print Anagrams Together.java
    Company Tags
    Morgan Stanley, Amazon, Microsoft, Snapdeal, D-E-Shaw, Goldman Sachs

---
+++ Print Anagrams Together.java
@@ -0,0 +1,67 @@
+gfg link: https://practice.geeksforgeeks.org/problems/print-anagrams-together/1
+
+QUES:
+Given an array of strings, return all groups of strings that are anagrams. The groups must be created in order of their appearance in the 
+original array. Look at the sample case for clarification.
+
+Note: The final output will be in lexicographic order.
+
+
+Example 1:
+
+Input:
+N = 5
+words[] = {act,god,cat,dog,tac}
+Output:
+act cat tac 
+god dog
+Explanation:
+There are 2 groups of
+anagrams "god", "dog" make group 1.
+"act", "cat", "tac" make group 2.
+Example 2:
+
+Input:
+N = 3
+words[] = {no,on,is}
+Output: 
+is
+no on
+Explanation:
+There are 2 groups of
+anagrams "is" makes group 1.
+"no", "on" make group 2.
+
+Your Task:
+The task is to complete the function Anagrams() that takes a list of strings as input and returns a list of groups such that each group 
+consists of all the strings that are anagrams.
+
+
+Expected Time Complexity: O(N*|S|*log|S|), where |S| is the length of the strings.
+Expected Auxiliary Space: O(N*|S|), where |S| is the length of the strings.
+
+
+Constraints:
+1<=N<=100
+
+1<=|S|<=10
+
+CODE:
+class Solution {
+    public List<List<String>> Anagrams(String[] str) {
+        // Code here
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : str){
+            char charArray[] = s.toCharArray();
+            Arrays.sort(charArray);
+            String sorted = new String(charArray);
+            
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new LinkedList<String>());
+            }
+            map.get(sorted).add(s);
+        }
+        return new LinkedList<>(map.values());
+    }
+}
+
