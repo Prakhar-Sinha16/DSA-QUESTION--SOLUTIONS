4eaf0fc4ff805c1be4e3d765cf9f2cae92a4c5f5
Prakhar Sinha <103882001+Prakhar-Sinha16@users.noreply.github.com>
2023-10-04 11:50:16 +0530

    Create Roman Number to Integer.java
    Company Tags
    Zoho, Amazon, Microsoft, Facebook, Twitter
    
    Topic Tags
    Strings, Data Structures

---
+++ Roman Number to Integer.java
@@ -0,0 +1,67 @@
+gfg link: https://practice.geeksforgeeks.org/problems/roman-number-to-integer3201/1
+
+QUES:
+Given a string in roman no format (s)  your task is to convert it to an integer . Various symbols and their values are given below.
+I 1
+V 5
+X 10
+L 50
+C 100
+D 500
+M 1000
+
+Example 1:
+
+Input:
+s = V
+Output: 5
+Example 2:
+
+Input:
+s = III 
+Output: 3
+Your Task:
+Complete the function romanToDecimal() which takes a string as input parameter and returns the equivalent decimal number. 
+
+Expected Time Complexity: O(|S|), |S| = length of string S.
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1<=roman no range<=3999
+
+CODE:
+class Solution {
+    // Finds decimal value of a given roman numeral
+    public int romanToDecimal(String str) {
+        // code here
+        // Create a map to store the values of Roman numerals
+        Map<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        // Iterate through the string in reverse order
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char currentChar = str.charAt(i);
+            int currentValue = romanValues.get(currentChar);
+
+            // Check if we need to subtract the previous value
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return result;
+    }
+}
